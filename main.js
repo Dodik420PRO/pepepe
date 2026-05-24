3fff6a444c5ac77a2e263ac8ae8ef7d0b18f5852
Dodik420PRO <155487556+Dodik420PRO@users.noreply.github.com>
2026-05-25 02:29:37 +0300

    fix: lookAt z=4 correct value for scene centering

--- main.js
+++ main.js
@@ -758,7 +758,7 @@ function loop() {
     frames = 0; lastT = now;
   }
   controls.update();
-   camX += (camXTarget - camX) * 0.08; camera.position.set(camX, CAM_Y, CAM_Z); camera.lookAt(camX * 0.3, CAM_LOOK_Y,40);
+   camX += (camXTarget - camX) * 0.08; camera.position.set(camX, CAM_Y, CAM_Z); camera.lookAt(camX * 0.3, CAM_LOOK_Y,4);
   tickers.forEach((fn) => fn(t));
   updateOverlays();
   updateMinimap();
